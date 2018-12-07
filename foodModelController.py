e4808899f42de5e47dfc453f94f6eed303610081
matancoh <matan.cohen@gmail.com>
2018-12-07 18:07:05 +0200

    adding API for all fields of food

--- foodModelController.py
+++ foodModelController.py
@@ -4,37 +4,51 @@ print(sys.executable)
 print(sys.version)
 import EnginesClient
 from flask import Flask, request, jsonify
-
-
-
 app = Flask(__name__)
 
-engines = None
-
+engine = EnginesClient.FoodEngineClient()
 
 @app.route('/api/productByName/<name>', methods=['GET'])
 def product_by_name(name):
-    if engines is None:
-        engine = EnginesClient.FoodEngineClient()
-
     res = engine.findProductByName(name)
     return jsonify(res.name)
 
+@app.route('/api/getIngredientsForProduct/<name>', methods=['GET'])
+def getIngredientsForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.ingredients)
 
-@app.route('/api/topFiveProducts/<name>', methods=['POST'])
-def product_by_name_top_five(name):
-    if engines is None:
-        engine = EnginesClient.FoodEngineClient()
+@app.route('/api/getEnergyForProduct/<name>', methods=['GET'])
+def getEnergyForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.energy)
 
-    res = engine.findProductByNameTopFive(name)
-    return jsonify(res)
+@app.route('/api/getProteinForProduct/<name>', methods=['GET'])
+def getProteinForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.protein)
 
-@app.route('/api/product/<id>', methods=['POST'])
+@app.route('/api/getFatForProduct/<name>', methods=['GET'])
+def getProteinForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.fat)
+
+@app.route('/api/getCarbohydrateForProduct/<name>', methods=['GET'])
+def getCarbohydrateForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.carbohydrate)
+
+@app.route('/api/getSugarsForProduct/<name>', methods=['GET'])
+def getSugarsForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.sugars)
+
+@app.route('/api/getSodiumForProduct/<name>', methods=['GET'])
+def getSodiumForProduct(name):
+    res = engine.findProductByName(name)
+    return jsonify(res.sodium)
+
+@app.route('/api/product/<id>', methods=['GET'])
 def product_by_id(id):
-    if engines is None:
-        engine = EnginesClient.FoodEngineClient()
-
     res = engine.getProductById(id)
     return jsonify(res)
-
-
