e3f126a7fbb3d4c68c83ae8e3eae9ab9a845db0c
asafm16 <41908304+asafm16@users.noreply.github.com>
2018-12-11 22:22:08 +0200

    added "__init__.py"

--- FoodEngine.py
+++ FoodEngine.py
@@ -154,7 +154,7 @@ class FoodEngine(object):
                     if counter == 1:
                         temp_inverted[word][index] = 1
                     else:
-                        temp_inverted[word][index] = 1 * counter
+                        temp_inverted[word][index] = math.pow(2, counter)
                         counter = counter - 1
                 else:
                     temp_inverted[word][index] = temp_inverted[word][index] + 1
